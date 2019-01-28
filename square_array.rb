8ecb83ea303713822076e677ea767242d66fb3ea
active-object-6703 <nerajno@gmail.com>
2019-01-28 14:25:28 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  squared_nums = []
+  array.each do |digit|
+  new_digit = digit*digit
+  squared_nums.push(new_digit)
 end
+end
+
+numbers = [1,2,3,4,5]
+square_array(numbers)
